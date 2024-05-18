ecaabc4ddc9b84600b95dba25b78adc40960831d
Tim Stills <timothy.stills@gmail.com>
2024-05-18 17:46:56 -0600

    Update.

--- tmp.py
+++ tmp.py
@@ -1,23 +1,21 @@
-# import the necessary packages
-from __future__ import print_function
 from imutils.video import VideoStream
 import numpy as np
 import argparse
 import imutils
 import time
 import cv2
-# construct the argument parse and parse the arguments
+
+# Construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
-ap.add_argument("-o", "--output", required=True, help="path to output video file")
-ap.add_argument("-p", "--picamera", type=int, default=-1, help="whether or not the Raspberry Pi camera should be used")
-ap.add_argument("-f", "--fps", type=int, default=20, help="FPS of output video")
 ap.add_argument("-c", "--codec", type=str, default="MJPG", help="codec of output video")
 args = vars(ap.parse_args())
+output_file = 'output.avi'
+fps = 20
 
 # initialize the video stream and allow the camera
 # sensor to warmup
 print("[INFO] warming up camera...")
-vs = VideoStream(usePiCamera=args["picamera"] > 0).start()
+vs = VideoStream(0).start()
 time.sleep(2.0)
 # initialize the FourCC, video writer, dimensions of the frame, and
 # zeros array
@@ -38,8 +36,7 @@ while True:
         # store the image dimensions, initialize the video writer,
         # and construct the zeros array
         (h, w) = frame.shape[:2]
-        writer = cv2.VideoWriter(args["output"], fourcc, args["fps"],
-            (w * 2, h * 2), True)
+        writer = cv2.VideoWriter(output_file, fourcc, fps, (w * 2, h * 2), True)
         zeros = np.zeros((h, w), dtype="uint8")
 
 # break the image into its RGB components, then construct the
