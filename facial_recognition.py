c3f7ad9dfdeea7f73ef0fbdb42b3153f2feb2fd9
Tim Stills <timothy.stills@gmail.com>
2024-12-08 16:25:03 -0700

    Update

--- facial_recognition.py
+++ facial_recognition.py
@@ -20,30 +20,29 @@ fps = 0
 def process_frame(frame, known_face_encodings, known_face_names):
     global face_locations, face_encodings, face_names
 
-    
     # Resize the frame using cv_scaler to increase performance (less pixels processed, less time spent)
     resized_frame = cv2.resize(frame, (0, 0), fx=(1/cv_scaler), fy=(1/cv_scaler))
-    
+
     # Convert the image from BGR to RGB colour space, the facial recognition library uses RGB, OpenCV uses BGR
     rgb_resized_frame = cv2.cvtColor(resized_frame, cv2.COLOR_BGR2RGB)
-    
+
     # Find all the faces and face encodings in the current frame of video
     face_locations = face_recognition.face_locations(rgb_resized_frame)
     face_encodings = face_recognition.face_encodings(rgb_resized_frame, face_locations, model='large')
-    
+
     face_names = []
     for face_encoding in face_encodings:
         # See if the face is a match for the known face(s)
         matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
         name = "Unknown"
-        
+
         # Use the known face with the smallest distance to the new face
         face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
         best_match_index = np.argmin(face_distances)
         if matches[best_match_index]:
             name = known_face_names[best_match_index]
         face_names.append(name)
-    
+
     return frame
 
 def draw_results(frame):
@@ -54,15 +53,15 @@ def draw_results(frame):
         right *= cv_scaler
         bottom *= cv_scaler
         left *= cv_scaler
-        
+
         # Draw a box around the face
         cv2.rectangle(frame, (left, top), (right, bottom), (244, 42, 3), 3)
-        
+
         # Draw a label with a name below the face
         cv2.rectangle(frame, (left -3, top - 35), (right+3, top), (244, 42, 3), cv2.FILLED)
         font = cv2.FONT_HERSHEY_DUPLEX
         cv2.putText(frame, name, (left + 6, top - 6), font, 1.0, (255, 255, 255), 1)
-    
+
     return frame
 
 def calculate_fps():
@@ -76,7 +75,6 @@ def calculate_fps():
     return fps
 
 def main():
-    
 
     # Load pre-trained face encodings
     print("[INFO] loading encodings...")
@@ -93,23 +91,23 @@ def main():
     while True:
         # Capture a frame from camera
         frame = picam2.capture_array()
-    
+
         # Process the frame with the function
         processed_frame = process_frame(frame, known_face_encodings, known_face_names)
-    
+
         # Get the text and boxes to be drawn based on the processed frame
         display_frame = draw_results(processed_frame)
-    
+
         # Calculate and update FPS
         current_fps = calculate_fps()
-    
+
         # Attach FPS counter to the text and boxes
         cv2.putText(display_frame, f"FPS: {current_fps:.1f}", (display_frame.shape[1] - 150, 30), 
                     cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
-    
+
         # Display everything over the video feed.
         cv2.imshow('Video', display_frame)
-    
+
         # Break the loop and stop the script if 'q' is pressed
         if cv2.waitKey(1) == ord("q"):
             break
